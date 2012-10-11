5f02a67c891ff37118e43d0f78106b08b4f6ef7f
Erik Erwitt <eerwitt@gmail.com>
2012-10-11 16:51:42 -0700

    downgraded jquery, updated some js with tracking

--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -1,1 +1,9 @@
-console.log "BLALALA"
+$ ->
+  gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.")
+  document.write unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")
+
+  try
+    pageTracker = _gat._getTracker("UA-663197-13")
+    pageTracker._trackPageview()
+  catch err
+    console.log err if console
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,14 @@
 
-console.log("BLALALA");
+$(function() {
+  var gaJsHost, pageTracker, _ref;
+  gaJsHost = (_ref = "https:" === document.location.protocol) != null ? _ref : {
+    "https://ssl.": "http://www."
+  };
+  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  try {
+    pageTracker = _gat._getTracker("UA-663197-13");
+    return pageTracker._trackPageview();
+  } catch (err) {
+    if (console) return console.log(err);
+  }
+});
