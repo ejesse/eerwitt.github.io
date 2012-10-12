09032f02e99eefa23f8ff0753b615b1f814d1530
Erik Erwitt <eerwitt@gmail.com>
2012-10-11 17:28:27 -0700

    removed calls to ga to keep jasmine from barfing

--- javascripts/main.coffee
+++
@@ -1,8 +0,0 @@
-$ ->
-  document.write unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")
-
-  try
-    pageTracker = _gat._getTracker("UA-663197-13")
-    pageTracker._trackPageview()
-  catch err
-    console.log err if typeof console isnt "undefined"
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,2 @@
 
-$(function() {
-  var pageTracker;
-  document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-  try {
-    pageTracker = _gat._getTracker("UA-663197-13");
-    return pageTracker._trackPageview();
-  } catch (err) {
-    if (typeof console !== "undefined") return console.log(err);
-  }
-});
+
