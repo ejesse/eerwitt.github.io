1a4b68f8b39c6f4c3c1568b91086db8630c84ca1
Erik Erwitt <eerwitt@gmail.com>
2012-10-11 17:03:14 -0700

    fixing up jasmine

--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -1,9 +1,8 @@
 $ ->
-  gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.")
-  document.write unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")
+  document.write unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")
 
   try
     pageTracker = _gat._getTracker("UA-663197-13")
     pageTracker._trackPageview()
   catch err
-    console.log err if console
+    console.log err if typeof console isnt "undefined"
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,11 @@
 
 $(function() {
-  var gaJsHost, pageTracker, _ref;
-  gaJsHost = (_ref = "https:" === document.location.protocol) != null ? _ref : {
-    "https://ssl.": "http://www."
-  };
-  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  var pageTracker;
+  document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
   try {
     pageTracker = _gat._getTracker("UA-663197-13");
     return pageTracker._trackPageview();
   } catch (err) {
-    if (console) return console.log(err);
+    if (typeof console !== "undefined") return console.log(err);
   }
 });
